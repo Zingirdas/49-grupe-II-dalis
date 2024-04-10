877c1b8d2eccf45d4be24520db8a61ba1b3ec095
Zingirdas <zingirdas@googlemail.com>
2024-04-10 08:56:45 +0300

    Atnaujinimai

---
+++ js1 - 18-03-2024/daugyba.js
@@ -0,0 +1,5 @@
+
+let x = Number(process.argv[2]);
+let y = Number(process.argv[3]);
+
+console.log(x * y);
---
+++ js1 - 18-03-2024/sudetis.js
@@ -0,0 +1,4 @@
+let x = Number(process.argv[2]);
+let y = Number(process.argv[3]);
+
+console.log(x + y);
